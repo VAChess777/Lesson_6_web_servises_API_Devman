7e5dffa16f517b99918928c9a82d4e529b794baa
VAChess777 <109584765+VAChess777@users.noreply.github.com>
2022-11-09 17:23:15 +0300

    step_7 of 17

--- main.py
+++ main.py
@@ -40,15 +40,92 @@ def download_random_comic(random_comic, path, random_number_comic):
     image = response.content
     with open(Path(f'{path}', f'{filename}'), 'wb') as file:
         file.write(image)
+    return filename
+
+
+def uploading_random_comic_to_server_vk(path, filename, upload_url_vk):
+    with open(Path(f'{path}', f'{filename}'), 'rb') as file:
+        files = {
+            'photo': file,
+        }
+        response = requests.post(upload_url_vk, files=files)
+        response.raise_for_status()
+        response_from_server_vk = response.json()
+        # print(response_from_server_vk)
+        return response_from_server_vk
+
+def saving_uploading_random_comic_in_album_vk(
+            access_token_vk,
+            response_from_server_vk,
+            owner_id,
+            group_id,
+            version_api_vk):
+    url = 'https://api.vk.com/method/photos.saveWallPhoto'
+    photo_from_server_vk = response_from_server_vk['photo']
+    server_number = response_from_server_vk['server']
+    hash_from_server_vk = response_from_server_vk['hash']
+    params = {
+        "access_token": access_token_vk,
+        'owner_id': owner_id,
+        'group_id': group_id,
+        "photo": photo_from_server_vk,
+        "server": server_number,
+        "hash": hash_from_server_vk,
+        "v": version_api_vk
+    }
+    response = requests.post(url, params=params)
+    response.raise_for_status()
+    image_id_from_response_server = response.json()['response'][0]['id']
+    return image_id_from_response_server
+
+
+def publish_random_comic_on_wall_vk(
+            image_id_from_response_server,
+            owner_id,
+            random_comic,
+            access_token_vk,
+            group_id,
+            version_api_vk,
+            upload_url_vk):
+    attachments = f'photo{owner_id}_{image_id_from_response_server}'
+    commentary_comic_picture = random_comic['alt']
+    params = {
+        "access_token": access_token_vk,
+        'owner_id': owner_id,
+        'friends_only': '1',
+        'attachments': attachments,
+        'from_group': '1',
+        'message': commentary_comic_picture,
+        "group_id": group_id,
+        "v": version_api_vk
+    }
+    response = requests.post(upload_url_vk, params=params)
+    response.raise_for_status()
+
 
 def main():
+    load_dotenv()
+    access_token_vk = os.environ['VK_ACCESS_TOKEN']
+    application_id = os.environ['VK_APPLICATION_ID']
+    upload_url_vk = os.environ['UPLOAD_URL_VK']
+    owner_id = os.environ['OWNER_ID']
+    group_id = os.environ['GROUP_ID']
+    version_api_vk = ['VERSION_API_VK']
     Path('Python Comics').mkdir(parents=True, exist_ok=True)
     path = 'Python Comics'
-    # get_last_comic()
-
     random_number_comic = get_random_number_comic()
     random_comic = get_random_comic(random_number_comic)
-    download_random_comic(random_comic, path, random_number_comic)
+    filename = download_random_comic(
+        random_comic,
+        path,
+        random_number_comic
+    )
+    response_from_server_vk = uploading_random_comic_to_server_vk(
+        path,
+        filename,
+        upload_url_vk
+    )
+
 
 if __name__ == '__main__':
 
